b04c4dce597e39a94f953f8a4f683697240c13f8
João Pedro <rjottaper@gmail.com>
2024-10-17 18:40:03 -0300

    Initial Commit

---
+++ Animae/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animae
+//
+//  Created by João Pedro Rocha on 17/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
