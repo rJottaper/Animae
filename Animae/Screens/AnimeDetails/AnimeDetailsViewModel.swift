1e05e8c61442154c875674e8f68823b0a819cabc
João Pedro <rjottaper@gmail.com>
2025-03-04 22:58:38 -0300

    Refactor: AnimeDetailsView to MVVM

---
+++ Animae/Screens/AnimeDetails/AnimeDetailsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AnimeDetailsViewModel.swift
+//  Animae
+//
+//  Created by João Pedro Rocha on 04/03/25.
+//
+
+import Foundation
---
+++ Animae/Services/AnimeRepository.swift
@@ -0,0 +1,8 @@
+//
+//  AnimeRepository.swift
+//  Animae
+//
+//  Created by João Pedro Rocha on 04/03/25.
+//
+
+import Foundation
